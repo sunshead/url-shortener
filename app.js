13c3a25fe622e3e6593adc7007e9fc636e681082
sunshead <sunshead@gmail.com>
2016-08-05 23:28:30 -0400

    deploy

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var mongodb = require("mongodb");
 var path = require('path');
-var config = require('./config');
+//var config = require('./config');
 var base62 = require('./base62.js'); //TBD
 //use body-parser middleware to handle post request
 var bodyParser = require('body-parser');
@@ -14,6 +15,32 @@ var url = require('./models/url');
 mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
 
 //serve static files from public folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -64,8 +91,4 @@ app.get('/:encoded_id', function(req, res) {
 		}
 	});
 
-});
-
-var server = app.listen(3000, function(){
-  console.log('Server listening on port 3000');
 });
